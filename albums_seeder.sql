3455123bd67cde2430d4c99688cea7f9868947e8
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-12-18 15:57:25 -0600

    sql where clause

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -96,6 +96,39 @@ VALUES ('Guns N Roses','Appetite for Destruction',1987,'Heavy metal, hard rock',
 
 
 
+-- INSERT INTO albums (artist, name, release_date, genre, sales)
+-- VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, Rock, R&B', 47.3)
+-- VALUES ('AC/DC', 'Back in Black', 1980, 'Hard Rock', 26.4)
+-- VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard Rock, Glam Rock, Heavy Metal', 21.7)
+-- VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive Rock', 24.2)
+-- VALUES ('Whitney Houston', 'The Bodyguard', 1992, 'R&B, Soul, Pop, Soundtrack', 28.4),
+-- VALUES ('Eagles', 'Greatest Hits(71-75)', 1976, 'Country Rock, Soft Rock, Folk Rock', 41.2),
+-- VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 'Disco', 27.9),
+-- VALUES ('Fleetwood Mac', 'Rumours', 1977, 'Soft Rock', 27.9),
+-- VALUES ('Shania Twain', 'Come On Over', 1997, 'Country, Pop', 29.6),
+-- VALUES ('Various Artists', 'Grease: TOS', 1978, 'Rock & Roll', 14.4),
+-- VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard Rock, Heavy Metal, Folk Rock', 29.0),
+-- VALUES ('Michael Jackson', 'Bad', 1987, 'Pop, Dance, Funk, Rock', 22.2),
+-- VALUES ('Alanis Morisette', 'Jagged Little Pill', 1995, 'Alternative Rock', 24.4),
+-- VALUES ('Celine Dion', 'Falling Into you', 1996, 'Pop, Soft Rock', 20.2),
+-- VALUES ('Eagles', 'Hotel California', 1976, 'Soft Rock', 31.5),
+-- VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 'Rock', 18.2),
+-- VALUES ('Various Artists', 'Dirty Dancing', 1987, 'Pop, Rock, R&B', 17.9),
+-- VALUES ('Celine Dion','Let''s Talk About Love', 1997 ,'Pop, Soft rock', 19.3),
+-- VALUES ('The Beatles','1',2000,'Rock', 23.4),
+-- VALUES ('Adele','21',2011,'Pop, soul', 25.3),
+-- VALUES ('The Beatles','Abbey Road',1969,'Rock', 14.4),
+-- VALUES ('Bruce Springsteen','Born in the U.S.A.',1984,'Rock', 19.6),
+-- VALUES ('Dire Straits','Brothers in Arms',1985,'Rock', 17.7),
+-- VALUES ('James Horner','Titanic: Music from the Motion Picture',1997,'Soundtrack', 18.1),
+-- VALUES ('Madonna','The Immaculate Collection',1990,'Pop, Dance', 19.2),
+-- VALUES ('Metallica','Metallica',1991,'Thrash metal, heavy metal', 25.2),
+-- VALUES ('Nirvana','Nevermind',1991,'Grunge, alternative rock', 16.7),
+-- VALUES ('Pink Floyd','The Wall',1979,'Progressive rock', 18.7),
+--
+--
+-- WVALUES ('Guns N Roses','Appetite for Destruction',1987,'Heavy metal, hard rock', 21.9);
 
 
 
+select * from albums;
